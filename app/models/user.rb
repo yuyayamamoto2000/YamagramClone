b434c126bda6645a791f8bc2c35101fe7c731cec
yuya yamamoto <yuya.yamamoto2000@gmail.com>
2020-09-14 23:00:10 +0900

    画像エラー作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   before_validation { email.downcase! }
   validates :name,  presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 },format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :image, presence: true
 
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
